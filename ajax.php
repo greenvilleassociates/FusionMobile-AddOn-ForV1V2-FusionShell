e2fe0de779162546be76299f2ddbf02e6a50d70d
greenvilleassociates <55986791+greenvilleassociates@users.noreply.github.com>
2020-10-20 14:47:23 -0400

    Add files via upload

---
+++ ajax.php
@@ -0,0 +1,11 @@
+<?
+
+$name = $_REQUEST['username'];
+$password = $_REQUEST['password'];
+
+$list = array('name'=>$name, 'password'=>$password);
+
+$c = json_encode($list);
+
+echo $c;
+?>
